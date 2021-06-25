b0121fa3ce435fb8e1285b9b3ff0424c1f6be64f
w-phil <wphillips892@gmail.com>
2021-06-25 14:41:41 -0400

    likes

---
+++ Likes/script.js
@@ -0,0 +1,26 @@
+var x = document.querySelector(".num-1");
+var y = document.querySelector(".num-1").innerHTML;
+var z = parseInt(y);
+
+function add1() {
+    x.innerText = z++;
+
+}
+
+var x2 = document.querySelector(".num-2");
+var y2 = document.querySelector(".num-2").innerHTML;
+var z2 = parseInt(y2);
+
+function add2() {
+    x2.innerText = z2++;
+
+}
+
+var x3 = document.querySelector(".num-3");
+var y3 = document.querySelector(".num-3").innerHTML;
+var z3 = parseInt(y3);
+
+function add3() {
+    x3.innerText = z3++;
+}
+
