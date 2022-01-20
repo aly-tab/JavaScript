330441272ba26d4a2897e5b4ce9056d3c2ee3fc0
Will Phillips <71557538+w-phil@users.noreply.github.com>
2022-01-20 11:19:37 -0500

    Delete Likes directory

--- Likes/script.js
+++
@@ -1,9 +0,0 @@
-var arr = [".num-1", ".num-2", ".num-3"];
-
-function add1(num) {
-    var x = document.querySelector(arr[num]);
-    var y = document.querySelector(arr[num]).innerHTML;
-    var z = parseInt(y);
-
-    x.innerText = z + 1;
-} 
